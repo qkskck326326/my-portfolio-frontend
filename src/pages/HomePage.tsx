fdb0b4062efb0110eae4610d9fa94700d850f57e
이건우 <qkskck3@gmail.com>
2025-06-14 17:01:38 +0900

    init: initial setup

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">홈 페이지</h1>
+      <p>보여지는 첫 화면입니다.</p>
+    </main>
+  )
+}
+export default HomePage;
