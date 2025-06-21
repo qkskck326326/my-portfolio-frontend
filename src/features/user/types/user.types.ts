ad40db434c824d0d3204d425b7bc636451d4495f
이건우 <qkskck3@gmail.com>
2025-06-21 12:59:44 +0900

    feat(user): UserProfileResponse 타입 정의 추가

---
+++ src/features/user/types/user.types.ts
@@ -0,0 +1,9 @@
+// src/features/user/types/user.types.ts
+export interface UserProfileResponse {
+  nickname: string;
+  userThumbnail: string;
+  email: string; 
+  github: string;
+  introduce: string;
+  birth: string; // ISO date string: 'yyyy-MM-dd'
+}
