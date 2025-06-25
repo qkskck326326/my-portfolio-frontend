e22afbedc4112ba83804e93ffb7df22316af108f
이건우 <qkskck3@gmail.com>
2025-06-25 12:32:17 +0900

    feat(portfolio): 포트폴리오 무한 스크롤을 위한 hook 추가

---
+++ src/features/portfolio/hooks/useInfiniteSearchPortfolio.ts
@@ -0,0 +1,26 @@
+// src/features/portfolio/hooks/useInfiniteSearchPortfolio.ts
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { PortfolioCard, PortfolioSearchRequest } from '../types/portfolio.types';
+import { Page } from '@/types/common.types';
+
+type Params = Omit<PortfolioSearchRequest, 'page'>;
+
+interface Options {
+  queryKeyBase: string;
+  queryFn: (params: PortfolioSearchRequest) => Promise<Page<PortfolioCard>>;
+  queryParams: Params;
+}
+
+export const useInfinitePortfolioQuery = (
+  { queryKeyBase, queryFn, queryParams }: Options,
+  deps: unknown[] = []
+) => {
+  return useInfiniteQuery<Page<PortfolioCard>, Error>({
+    queryKey: [queryKeyBase, queryFn, queryParams.keyword, queryParams.tags, queryParams.sort, ...deps],
+    queryFn: ({ pageParam = 0 }) =>
+      queryFn({ ...queryParams, page: pageParam as number }),
+    getNextPageParam: (lastPage) =>
+      lastPage.last ? undefined : lastPage.number + 1,
+    initialPageParam: 0,
+  });
+};
