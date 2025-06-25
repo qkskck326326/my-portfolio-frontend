1b737b6d2945c7570295f229151f91097ee8dfb3
이건우 <qkskck3@gmail.com>
2025-06-25 12:27:49 +0900

    feat(portfolio): 포트폴리오 관련 타입 정의 파일 생성

---
+++ src/features/portfolio/types/portfolio.types.ts
@@ -0,0 +1,55 @@
+// src/features/portfolio/types/portfolio.types.ts
+import { CommonResponse, Page } from '@/types/common.types';
+
+// 포트폴리오 카드
+export interface PortfolioCard {
+    id: number;
+    thumbnail: string;
+    title: string;
+    likeCount: number;
+    createdAt: string; // ISO date string: 'yyyy-MM-dd'
+    nickname: string;
+    authorId: number;
+}
+
+// 포트폴리오 카드 목록 응답
+export type PortfolioCardListResponse = CommonResponse<Page<PortfolioCard>>;
+
+
+// 포트폴리오 상세 정보
+export interface PortfolioDetail {
+  id: number;
+  title: string;
+  content: string;
+  thumbnail: string;
+  authorNickname: string;
+  likeCount: number;
+  tags: string[];
+}
+
+// 포트폴리오 상세 정보 응답
+export type PortfolioDetailResponse = CommonResponse<PortfolioDetail>;
+
+
+// 정렬기준 필드 및 sort (아래 요청 내에 들어감)
+export interface SortRequest {
+  field: string; // 정렬 필드명
+  direction: 'ASC' | 'DESC';
+}
+
+// 포트폴리오 색인 요청
+export interface PortfolioSearchRequest {
+  keyword?: string;
+  tags?: string[];
+  page?: number;
+  size?: number;
+  sort?: SortRequest[];
+}
+
+// 포트폴리오 생성 요청
+export type CreatePortfolioRequest = {
+  title: string;
+  thumbnail?: string;
+  content: string;
+  tags?: string[];
+};
