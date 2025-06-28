cd6395d05033f7ea18744b7cf52b8883447e9978
이건우 <qkskck3@gmail.com>
2025-06-28 17:10:33 +0900

    feat(portfolio): 포트폴리오 작성 페이지(PortfolioWritePage) 추가

---
+++ src/pages/PortfolioWritePage.tsx
@@ -0,0 +1,12 @@
+// src/pages/PortfolioWritePage.tsx
+import PortfolioForm from '@/features/portfolio/components/PortfolioForm';
+
+const PortfolioWritePage = () => {
+  return (
+    <div className="max-w-4xl mx-auto mt-12 p-6 bg-white rounded-lg shadow space-y-6">
+      <PortfolioForm/>
+    </div>
+  );
+};
+
+export default PortfolioWritePage;
