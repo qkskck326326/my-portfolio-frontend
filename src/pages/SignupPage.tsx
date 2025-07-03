d371d9b93e072624ad06692dba970312fe121eed
이건우 <qkskck3@gmail.com>
2025-07-03 11:51:23 +0900

    feat(user): 회원가입 페이지(SignupPage) 컴포넌트 생성

---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,12 @@
+// src/pages/SignupPage.tsx
+import SignupForm from '@/features/user/components/SignupForm';
+
+const SignupPage = () => {
+  return (
+    <main className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <SignupForm />
+    </main>
+  );
+};
+
+export default SignupPage;
