6bf8d37aeb61bccfdd65a8ed6cff2842483356c6
이건우 <qkskck3@gmail.com>
2025-07-03 11:49:42 +0900

    feat(user): UserProfile 타입 확장 및 요청 DTO(UserProfileEditRequest, SignupRequest) 정의

--- src/features/user/types/user.types.ts
+++ src/features/user/types/user.types.ts
@@ -1,9 +1,35 @@
 // src/features/user/types/user.types.ts
-export interface UserProfile {
+export interface UserProfileEditRequest {
   nickname: string;
   userThumbnail: string;
   email: string; 
   github: string;
   introduce: string;
   birth: string; // ISO date string: 'yyyy-MM-dd'
+}
+
+export interface UserProfile {
+  userThumbnail: string;
+  nickname: string;
+  slug: string;
+  email: string;
+  github?: string;
+  introduce?: string;
+  birth?: string; // ISO date string: 'yyyy-MM-dd'
+  tags: Tag[];
+}
+
+export interface Tag{
+  name: string;
+  amount: number;
+}
+
+export interface SignupRequest {
+  email: string;
+  password: string;
+  nickname: string;
+  userThumbnail?: string;
+  github?: string;
+  introduce?: string;
+  birth?: string;
 }
