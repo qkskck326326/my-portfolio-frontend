ebec92d2677d809650216126ed0afa3a5a0a589f
이건우 <qkskck3@gmail.com>
2025-07-03 12:22:22 +0900

    feat(user): slug에 따라서 분기 전환 가능하도록 slug 입력받도록 수정

--- src/features/user/components/UserProfileContainer.tsx
+++ src/features/user/components/UserProfileContainer.tsx
@@ -1,14 +1,26 @@
+// src/features/user/components/UserProfileContainer.tsx
 import { useState } from 'react';
 import UserProfileViewer from './UserProfileViewer';
 import UserProfileEditor from './UserProfileEditor';
+import { useUserProfile } from '../hooks/useUserProfile';
 
-const UserProfileContainer = () => {
+interface Props {
+  slug: string;
+}
+
+const UserProfileContainer = ({ slug }: Props) => {
   const [isEditing, setIsEditing] = useState(false);
+  const { data, isLoading, error } = useUserProfile({ slug });
 
   return isEditing ? (
-    <UserProfileEditor onCancel={() => setIsEditing(false)} onComplete={() => setIsEditing(false)} />
+    <UserProfileEditor slug={slug} onCancel={() => setIsEditing(false)} onComplete={() => setIsEditing(false)} />
   ) : (
-    <UserProfileViewer onEdit={() => setIsEditing(true)} />
+    <UserProfileViewer 
+      slug={slug} 
+      data={data}
+      isLoading={isLoading}
+      error={error}
+    onEdit={() => setIsEditing(true)} />
   );
 };
 
