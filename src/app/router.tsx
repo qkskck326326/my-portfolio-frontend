ae1643feb6f4a2f3d3a57c51fd912afcb9ea8891
이건우 <qkskck3@gmail.com>
2025-08-03 19:29:30 +0900

    feat(router): 기존의 마이 페이지 -> slug 기반 유저 페이지로 라우팅 수정 & 에러 및 회원가입 페이지 라우팅 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,6 +6,7 @@ import ErrorPage from '@/pages/ErrorPage';
 import MyPage from '@/pages/MyPage';
 import PortfolioWritePage from '@/pages/PortfolioWritePage';
 import PortfolioDetailPage from '@/pages/PortfolioDetailPage';
+import SignupPage from '@/pages/SignupPage';
 import AppLayout from '@/components/Layout/AppLayout';
 
 export const router = createBrowserRouter([
@@ -15,10 +16,12 @@ export const router = createBrowserRouter([
     errorElement: <ErrorPage />,       // 공통 에러 페이지
     children: [
       { index: true, element: <HomePage /> },
+      { path: '/error', element: <ErrorPage /> },
       { path: 'login', element: <LoginPage /> },
-      { path: 'mypage', element: <MyPage />, },
+      { path: '/user/:slug', element: <MyPage />, },
       { path: "/portfolio/write", element: <PortfolioWritePage />},
-      { path: '/portfolio/:portfolioId', element: <PortfolioDetailPage />}
+      { path: '/portfolio/:portfolioId', element: <PortfolioDetailPage />},
+      { path: 'signup', element: <SignupPage /> }
     ],
   },
 ]);
