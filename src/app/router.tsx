46e61f0eaeaa01ba82667ed831689adddbb5e164
이건우 <qkskck3@gmail.com>
2025-08-03 22:28:50 +0900

    feat(router): 좋아요 표시한 포트폴리오 페이지 라우팅 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,6 +6,7 @@ import ErrorPage from '@/pages/ErrorPage';
 import MyPage from '@/pages/MyPage';
 import PortfolioWritePage from '@/pages/PortfolioWritePage';
 import PortfolioDetailPage from '@/pages/PortfolioDetailPage';
+import MyLikedPortfolioPage from '@/pages/MyLikedPortfolioPage';
 import SignupPage from '@/pages/SignupPage';
 import AppLayout from '@/components/Layout/AppLayout';
 
@@ -19,6 +20,7 @@ export const router = createBrowserRouter([
       { path: '/error', element: <ErrorPage /> },
       { path: 'login', element: <LoginPage /> },
       { path: '/user/:slug', element: <MyPage />, },
+      { path: '/my/likes', element: <MyLikedPortfolioPage /> }, // 자신의 좋아요 표시한 포트폴리오 페이지
       { path: "/portfolio/write", element: <PortfolioWritePage />},
       { path: '/portfolio/:portfolioId', element: <PortfolioDetailPage />},
       { path: 'signup', element: <SignupPage /> }
