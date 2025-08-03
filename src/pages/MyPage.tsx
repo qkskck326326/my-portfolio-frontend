1945b73c27536075edb20d6b5abc50e0e4718c1f
이건우 <qkskck3@gmail.com>
2025-08-03 19:27:19 +0900

    feat(user): 마이페이지를 slug 기반 동적 유저 페이지로 수정

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,10 +1,16 @@
+// src/pages/MyPage.tsx
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import UserProfileContainer from '@/features/user/components/UserProfileContainer';
 
 const MyPage = () => {
+  const { slug } = useParams();
+
+  if (!slug) return <div>존재하지 않는 유저입니다.</div>;
+
   return (
-    <div className="min-h-screen flex justify-center items-start pt-10 bg-white">
-      <UserProfileContainer />
+    <div className="min-h-screen flex justify-center items-start bg-white">
+      <UserProfileContainer slug={slug} />
     </div>
   );
 };
